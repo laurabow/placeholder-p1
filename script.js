028fbc47f151fc1663e33642a44d9af12bfd477f
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2021-12-13 10:25:40 -0500

    added dom and body elements to html, started js script

---
+++ script.js
@@ -0,0 +1,6 @@
+const DOMAIN = "https://api.spoonacular.com/recipes/random";
+const apiKey = "6828013b9cb841fa99a28230ff495cab";
+const baseUrl = "https://api.spoonacular.com/recipes/random?apiKey=${apiKey}";
+
+const recipeButton = document.querySelector("#recipe-button");
+
