5c628e4dd40e250c3c1178fd5cb41d29ad13e1d0
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2021-12-13 13:49:03 -0500

    fixed api url with workaround for corb issue, removed api script line in html

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const DOMAIN = "https://api.spoonacular.com/recipes/random";
-const apiKey = "6828013b9cb841fa99a28230ff495cab";
-const baseUrl = "https://api.spoonacular.com/recipes/random?apiKey=${apiKey}";
+// const DOMAIN = "https://api.spoonacular.com/recipes/random";
+// const apiKey = "6828013b9cb841fa99a28230ff495cab";
+// const baseUrl = "https://api.spoonacular.com/recipes/random?apiKey=${apiKey}";
 
 const recipeButton = document.querySelector("#recipe-button");
 
 
-// async function showRecipe() {
-//   let res = await axios.get("https://api.spoonacular.com/recipes/random?apiKey=${apiKey}")
-// }
+async function fetchRecipe() {
+  try {
+    const apiKey = "6828013b9cb841fa99a28230ff495cab";
+    const baseUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
+    let res = await axios.get(`https://boiling-mountain-84087.herokuapp.com/${baseUrl}`)
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchRecipe();
