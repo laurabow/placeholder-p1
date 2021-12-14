70f6465da13ad171105d31b19b8f5585c52f087e
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2021-12-14 09:14:22 -0500

    ingredients list individually now, need to find a way to remove empty spots for smaller recipes

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ async function fetchIngredients() {
     const ingredients = [`${recipe.strMeasure1} ${recipe.strIngredient1}`, `${recipe.strMeasure2} ${recipe.strIngredient2}`, `${recipe.strMeasure3} ${recipe.strIngredient3}`, `${recipe.strMeasure4} ${recipe.strIngredient4}`, `${recipe.strMeasure5} ${recipe.strIngredient5}`, `${recipe.strMeasure6} ${recipe.strIngredient6}`, `${recipe.strMeasure7} ${recipe.strIngredient7}`, `${recipe.strMeasure8} ${recipe.strIngredient8}`, `${recipe.strMeasure9} ${recipe.strIngredient9}`, `${recipe.strMeasure10} ${recipe.strIngredient10}`, `${recipe.strMeasure11} ${recipe.strIngredient11}`, `${recipe.strMeasure12} ${recipe.strIngredient12}`, `${recipe.strMeasure13} ${recipe.strIngredient13}`, `${recipe.strMeasure14} ${recipe.strIngredient14}`, `${recipe.strMeasure15} ${recipe.strIngredient15}`, `${recipe.strMeasure16} ${recipe.strIngredient16}`, `${recipe.strMeasure17} ${recipe.strIngredient17}`, `${recipe.strMeasure18} ${recipe.strIngredient18}`, `${recipe.strMeasure19} ${recipe.strIngredient19}`, `${recipe.strMeasure20} ${recipe.strIngredient20}`];
     for (let i = 0; i < ingredients.length; i++) {
       const li = document.createElement("li");
-      li.innerText = ingredients;
+      li.innerText = ingredients[i];
       randomRecipe.appendChild(li);
     };
   } catch (error) {
@@ -46,6 +46,11 @@ async function fetchIngredients() {
   }
 }
 fetchIngredients();
+
+// appending ingredients to dom:
+// const li = document.createElement("li");
+// li.innerText = ingredients;
+// randomRecipe.appendChild(li);
 
 // Instructions:
 async function fetchInstructions() {
