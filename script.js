84169a0a159bd417a05768d5dd9fc85628c90dc8
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2021-12-14 15:35:05 -0500

    saved, working on removing previous image

--- script.js
+++ script.js
@@ -38,8 +38,7 @@ async function fetchRecipeName() {
       }
       // console.log(ingredient, measurement);
     }
-    // fix these so they list on individual bullet points:
-    // const ingredients = [`${recipe.strMeasure1} ${recipe.strIngredient1}`, `${recipe.strMeasure2} ${recipe.strIngredient2}`, `${recipe.strMeasure3} ${recipe.strIngredient3}`, `${recipe.strMeasure4} ${recipe.strIngredient4}`, `${recipe.strMeasure5} ${recipe.strIngredient5}`, `${recipe.strMeasure6} ${recipe.strIngredient6}`, `${recipe.strMeasure7} ${recipe.strIngredient7}`, `${recipe.strMeasure8} ${recipe.strIngredient8}`, `${recipe.strMeasure9} ${recipe.strIngredient9}`, `${recipe.strMeasure10} ${recipe.strIngredient10}`, `${recipe.strMeasure11} ${recipe.strIngredient11}`, `${recipe.strMeasure12} ${recipe.strIngredient12}`, `${recipe.strMeasure13} ${recipe.strIngredient13}`, `${recipe.strMeasure14} ${recipe.strIngredient14}`, `${recipe.strMeasure15} ${recipe.strIngredient15}`, `${recipe.strMeasure16} ${recipe.strIngredient16}`, `${recipe.strMeasure17} ${recipe.strIngredient17}`, `${recipe.strMeasure18} ${recipe.strIngredient18}`, `${recipe.strMeasure19} ${recipe.strIngredient19}`, `${recipe.strMeasure20} ${recipe.strIngredient20}`];
+    // Ingredients:
     const ingredients = [];
     for (let i = 0; i < ingredient.length; i++) {
       let str = `${measurement[i]} ${ingredient[i]}`;
@@ -54,12 +53,12 @@ async function fetchRecipeName() {
       }
       // console.log(typeof ingredients[i]);
     };
-    // end ingredients
+    // instructions:
     const instructions = recipe.strInstructions;
     const p = document.createElement("p");
     p.innerText = instructions;
     randomRecipe.appendChild(p);
-    // end instructions
+    // image:
     const recipePic = recipe.strMealThumb;
     const img = document.createElement("img");
     img.src = recipePic;
@@ -167,5 +166,4 @@ recipeButton.addEventListener("click", handleClick);
 // removes previous recipe when button clicked again:
 function removeRecipe() {
   randomRecipe.innerHTML = "";
-  imageDiv.src = "";
 }
