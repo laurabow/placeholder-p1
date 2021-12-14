2dc2c430e40cbb76064f30b6b17a7170643f01e4
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2021-12-14 12:02:03 -0500

    tried fixing empty bullet point issue to no avail

--- script.js
+++ script.js
@@ -39,6 +39,11 @@ async function fetchIngredients() {
     // console.log(recipe);
     // fix these so they list on individual bullet points:
     const ingredients = [`${recipe.strMeasure1} ${recipe.strIngredient1}`, `${recipe.strMeasure2} ${recipe.strIngredient2}`, `${recipe.strMeasure3} ${recipe.strIngredient3}`, `${recipe.strMeasure4} ${recipe.strIngredient4}`, `${recipe.strMeasure5} ${recipe.strIngredient5}`, `${recipe.strMeasure6} ${recipe.strIngredient6}`, `${recipe.strMeasure7} ${recipe.strIngredient7}`, `${recipe.strMeasure8} ${recipe.strIngredient8}`, `${recipe.strMeasure9} ${recipe.strIngredient9}`, `${recipe.strMeasure10} ${recipe.strIngredient10}`, `${recipe.strMeasure11} ${recipe.strIngredient11}`, `${recipe.strMeasure12} ${recipe.strIngredient12}`, `${recipe.strMeasure13} ${recipe.strIngredient13}`, `${recipe.strMeasure14} ${recipe.strIngredient14}`, `${recipe.strMeasure15} ${recipe.strIngredient15}`, `${recipe.strMeasure16} ${recipe.strIngredient16}`, `${recipe.strMeasure17} ${recipe.strIngredient17}`, `${recipe.strMeasure18} ${recipe.strIngredient18}`, `${recipe.strMeasure19} ${recipe.strIngredient19}`, `${recipe.strMeasure20} ${recipe.strIngredient20}`];
+
+    // const filterIng = ingredients.filter(el => {
+    //   return el != null && el != "";
+    // });
+
     for (let i = 0; i < ingredients.length; i++) {
       const li = document.createElement("li");
       li.innerText = ingredients[i];
